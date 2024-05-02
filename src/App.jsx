c72565ce75e1835085c01727901db65d759c2f28
Shanmukh459 <saikrrish459@gmail.com>
2024-05-02 13:39:54 -0700

    added useData hook to get data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useEffect, useState } from 'react'
 import { csv, scaleLinear, extent } from 'd3';
-
-const csvUrl =   'https://gist.githubusercontent.com/Shanmukh459/b5d83c8b7334616ceb7d9bfba7ffeb24/raw/c2a883e845ddae11c612c03823ee403e1f4d12ed/Iris.csv';
+import { useData } from './useData';
 
 const width = 960
 const height = 500
@@ -14,18 +12,8 @@ const margin = {
 }
 
 function App() {
-  const [data, setData] = useState()
-
-  useEffect(() => {
-    const row = (d) => {
-      d.sepal_length = +d.sepal_length
-      d.petal_length = +d.petal_length
-      d.sepal_width = +d.sepal_width
-      d.petal_width = +d.petal_width
-      return d
-    }
-    csv(csvUrl, row).then(setData)
-  }, [])
+  const data = useData()
+  
 
   if(!data) {
     return <h1>Loading...</h1>
